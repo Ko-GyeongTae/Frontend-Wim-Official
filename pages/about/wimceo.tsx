bf60231f6a072773d21f1e4931dbf487594eb1fc
KoGyeongtae <kokt0203@naver.com>
2021-08-07 04:17:53 +0900

    MADD> wimceo

---
+++ pages/about/wimceo.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Image from 'next/image'
+import React from "react";
+import styles from './wimceo.module.css';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Head>
+                <title>WIM | CEO</title>
+                <link rel="icon" href="/wimicon.svg" />
+            </Head>
+            <div className={styles.Container}>
+                <div className={styles.MainBox}>
+                    <div className={styles.Header}>
+                        <Image src={'/ceo/profile.jpeg'} width={300} height={300} />
+                        <div className={styles.HeaderText}>
+                            <h1>전우진 CEO</h1>
+                            <h2>경북대학교 학생 홍보대사</h2>
+                        </div>
+                    </div>
+                    <div className={styles.Body1}>
+                        <h3>저는 도전적인 성격의 소유자입니다.</h3>
+                        <h3>새로운 일은 신나는 일! 주저없이 뛰어듭니다.</h3>
+                    </div>
+                    <div className={styles.Body2}>
+                        <h3>저는 매 순간 최선을 다하여</h3>
+                        <h3>제가 원하는 것을 얻고자 끊임없이 노력합니다.</h3>
+                    </div>
+                    <div className={styles.Body3}>
+                        <h3>저는 사람들을 만나는 것을 좋아하며,</h3>
+                        <h3>형성된 인적 네트워크로 새로운 가치를 만들어내고자 합니다.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
