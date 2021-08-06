821e3097009388e250af6cc1081b804737b2847e
KoGyeongtae <kokt0203@naver.com>
2021-08-06 23:29:34 +0900

    <EDIT> total layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from 'next/app'
+import React from 'react'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </Head>
+      <div style={{ width: '1200 auto', margin: 0, height: 'auto', alignContent: 'center'}}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
