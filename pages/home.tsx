d70fcffd702a08d8b16d99486c25d590293c24c6
KoGyeongtae <kokt0203@naver.com>
2021-08-06 23:29:16 +0900

    <ADD> Home page

---
+++ pages/home.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>WIM | HOME</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Home</h1>
+    </div>
+  )
+}
