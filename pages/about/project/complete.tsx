e75f010280c545d0994f60b64969a1d4ece186de
KoGyeongtae <kokt0203@naver.com>
2021-08-07 15:52:46 +0900

    <ADD> project detail Pages

---
+++ pages/about/project/complete.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>complete</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ pages/about/project/plan.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    
+    return (
+        <div>
+            <h1>Plan</h1>
+        </div>
+    )
+}
+
+export default Home;
