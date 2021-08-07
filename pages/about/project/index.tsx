647fd9c150e5df034dd3b726ac843865215818b8
KoGyeongtae <kokt0203@naver.com>
2021-08-07 15:39:03 +0900

    <ADD> project category

--- pages/about/project/index.tsx
+++ pages/about/project/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from 'next/head';
+import style from './index.module.css';
 
 const Home = () => {
 
@@ -9,8 +10,16 @@ const Home = () => {
                 <title>WIM | Project</title>
                 <link rel="icon" href="/wimicon.svg" />
             </Head>
-            <div>
-                <h1>Project</h1>
+            <div className={style.Container}>
+                <div className={style.Category}>
+                    <h1>Wim의 완성된 프로젝트</h1>
+                </div>
+                <div className={style.Category}>
+                    <h1>Wim의 진행중인 프로젝트</h1>
+                </div>
+                <div className={style.Category}>
+                    <h1>Wim의 계획중인 프로젝트</h1>
+                </div>
             </div>
         </div>
     )
