638cfe4d3755562d5370641301312330f001c588
KoGyeongtae <kokt0203@naver.com>
2021-08-07 15:50:10 +0900

    <ADD> Link: )

--- pages/about/project/index.tsx
+++ pages/about/project/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from 'next/head';
 import style from './index.module.css';
+import Link from "next/link";
 
 const Home = () => {
 
@@ -11,15 +12,21 @@ const Home = () => {
                 <link rel="icon" href="/wimicon.svg" />
             </Head>
             <div className={style.Container}>
+                <Link href={'/about/project/complete'} passHref>
                 <div className={style.Category}>
                     <h1>Wim의 완성된 프로젝트</h1>
                 </div>
+                </Link>
+                <Link href={'/about/project/proceed'} passHref>
                 <div className={style.Category}>
                     <h1>Wim의 진행중인 프로젝트</h1>
                 </div>
+                </Link>
+                <Link href={'/about/project/plan'} passHref>
                 <div className={style.Category}>
                     <h1>Wim의 계획중인 프로젝트</h1>
                 </div>
+                </Link>
             </div>
         </div>
     )
